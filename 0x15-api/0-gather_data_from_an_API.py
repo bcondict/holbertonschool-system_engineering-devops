5d84d8e56360ffad9fdf5ec8c5e96b44b4338966
Jesus <jesus.junc10@gmail.com>
2022-09-08 14:56:29 -0500

    [OK] 0 task

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+using this REST API, for a given employee ID
+returns information about his/her list progress.
+"""
+
+if __name__ == "__main__":
+
+    import requests
+    from sys import argv
+
+    # base
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    emp_id = argv[1]
+
+    # complex
+    users = requests.get('{}{}'.format(url, emp_id)).json()
+    todos = requests.get('{}{}/todos'.format(url, emp_id)).json()
+
+    # to print
+    done_task = []
+    for task in todos:
+        if task['completed'] is True:
+            done_task.append(task['title'])
+
+    print('Employee {}  is done with tasks ({}/{})'.format(
+        users['name'], len(done_task), len(todos)))
+
+    for task_title in done_task:
+        print('\t {}'.format(task_title))
